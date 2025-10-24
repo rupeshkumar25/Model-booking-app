cb821f7d795bbc91956b9baba69ce772df99d7c4
Rupesh kumar <53491959+rupeshkumar25@users.noreply.github.com>
2025-10-25 01:23:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Admin from './admin'
-import './index.css'
-
+import Profile from './Profile'
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>} />
-        <Route path='/admin' element={<Admin/>} />
+        <Route path='/profile/:id' element={<Profile/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
